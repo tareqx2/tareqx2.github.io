6ff9cb6bee3c627df4bae83612c50885184d81d1
Tareq Perez <tareqx3@gmail.com>
2015-02-16 22:26:50 -0600

    check-in for website

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python Src/api.py
---
+++ Src/story_generator.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	function getStory(){
+		$('.loading').show();
+		$.ajax({
+		  url: "http://story-generator.herokuapp.com/api/v1.0/story"
+		})
+		.done(function(html){
+			var json = jQuery.parseJSON(html);
+			var description = $('.description');
+			var synopsis = $('.synopsis p');
+			var title = $('.title');
+			title[0].textContent = json["Name"];
+			description[0].textContent = json["description"];
+			if(json["storyline"])
+				synopsis[0].textContent = json["storyline"];
+			else
+				synopsis[0].textContent = "Sorry, no synopsis available.";
+			$('.loading').hide();
+		});
+	}
+	getStory();
+
+	$('#generate').on("click",function(e){
+		getStory();
+		$("#synopsis").removeClass("hidden");
+		$(".synopsis").addClass("hidden");
+	});
+	$("#synopsis").on("click",function(e){
+		$("#synopsis").toggleClass("hidden");
+		$(".synopsis").toggleClass("hidden");
+
+	})
+});
